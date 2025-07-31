2799b1445d3893048bf7f59c8383e7fc8321131a
github-laziji <c_smtp@163.com>
2025-07-31 20:48:36 +0800

    init

--- src/main/java/org/laziji/blindchess/Board.java
+++ src/main/java/org/laziji/blindchess/Board.java
@@ -1,5 +1,12 @@
 package org.laziji.blindchess;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 public class Board {
 
     private final char[][] map = new char[][]{
@@ -21,19 +28,69 @@ public class Board {
 
     }
 
-    public void activeByEnCmd(String cmd) {
-        int ox = cmd.charAt(5) - 'a';
-        int oy = cmd.charAt(6) - '0';
-        int nx = cmd.charAt(7) - 'a';
-        int ny = cmd.charAt(8) - '0';
+    public void active(int ox, int oy, int nx, int ny) {
         map[ny][nx] = map[oy][ox];
         map[oy][ox] = ' ';
         wb = 1 - wb;
     }
 
+    public void activeByEnCmd(String cmd) {
+        int ox = cmd.charAt(5) - 'a';
+        int oy = cmd.charAt(6) - '0';
+        int nx = cmd.charAt(7) - 'a';
+        int ny = cmd.charAt(8) - '0';
+        active(ox, oy, nx, ny);
+    }
+
     public void activeByChCmd(String cmd) {
-        // 炮8平5
+
         cmd = cmd.replaceAll("\\s", "");
+        if (cmd.length() != 4) {
+            System.out.printf("ERROR: [%s]走棋不符合规范\n", cmd);
+            return;
+        }
+        int ox = -1;
+        int oy = -1;
+        // 炮8平5
+        if (cmd.matches("^[\\u4E00-\\u9FA5][1-9]..$")) {
+            String c = getPingYin(String.valueOf(cmd.charAt(0)));
+            ox = Integer.parseInt(String.valueOf(cmd.charAt(1))) - 1;
+            for (oy = 0; oy < 10; oy++) {
+                if (map[oy][ox] == 'K' && "shuai".equals(c)
+                        || map[oy][ox] == 'A' && "shi".equals(c)
+                        || map[oy][ox] == 'B' && "xiang".equals(c)
+                        || map[oy][ox] == 'N' && "ma".equals(c)
+                        || map[oy][ox] == 'R' && ("ju".equals(c) || "che".equals(c))
+                        || map[oy][ox] == 'C' && "pao".equals(c)
+                        || map[oy][ox] == 'P' && "bing".equals(c)) {
+                    break;
+                }
+            }
+            if (oy == 10) {
+                System.out.printf("ERROR: [%c]路线上没有棋子[%s]\n", cmd.charAt(1), c);
+                return;
+            }
+        } else if (cmd.matches("^([前中后][\\u4E00-\\u9FA5]|[一二三四五]兵)..$")) {
+
+        } else {
+            System.out.printf("ERROR: [%s]走棋不符合规范\n", cmd);
+            return;
+        }
+
+        int nx = ox;
+        int ny = oy;
+        if (cmd.matches("^..[进平退][1-9]$")) {
+            if (cmd.charAt(3) == '进') {
+                ny += Integer.parseInt(String.valueOf(cmd.charAt(3)));
+            } else if (cmd.charAt(3) == '平') {
+                nx = Integer.parseInt(String.valueOf(cmd.charAt(3))) - 1;
+            } else if (cmd.charAt(3) == '进') {
+                ny -= Integer.parseInt(String.valueOf(cmd.charAt(3)));
+            }
+            active(ox, oy, nx, ny);
+        } else {
+            System.out.printf("ERROR: [%s]走棋不符合规范\n", cmd);
+        }
     }
 
     public String getStatus() {
@@ -63,5 +120,28 @@ public class Board {
 
     public String getWb() {
         return wb == 0 ? "w" : "b";
+    }
+
+    private String getPingYin(String val) {
+        try {
+            HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+            format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+            format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+            format.setVCharType(HanyuPinyinVCharType.WITH_V);
+            StringBuilder output = new StringBuilder();
+            char[] input = val.trim().toCharArray();
+            for (char c : input) {
+                if (!Character.toString(c).matches("[\\u4E00-\\u9FA5]+")) {
+                    String[] temp = PinyinHelper.toHanyuPinyinStringArray(c, format);
+                    output.append(temp[0]);
+                    continue;
+                }
+                output.append(c);
+            }
+            return output.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 }
